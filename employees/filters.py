fd850e53b92fe6606da2487f76fb08644d41c218
evgeniya35 <evgeniya35@yandex.ru>
2022-03-04 09:17:47 +0300

    Add all fields to filter

--- employees/filters.py
+++ employees/filters.py
@@ -3,6 +3,8 @@ import django_filters
 from .models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
+    # parent = django_filters.CharFilter()
+
     class Meta:
         model = Employee
-        fields = ['name', 'position']
+        fields = ['name', 'parent', 'position', 'emp_date', 'salary'] 
